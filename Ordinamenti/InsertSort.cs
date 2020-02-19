af105529e336fd75ff66dd79f67ee2d17dd570c4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-19 13:12:11 +0100

    Add project files.

---
+++ Ordinamenti/InsertSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MetodiOrdinamenti
+{
+    public class InsertSort
+    {
+        public static int[] Insert_Sort (int[]a)
+        {
+            int flag;
+            int val;
+            for (int i = 1; i < a.Length; i++)
+            {
+                val = a[i];
+                flag = i - 1;
+                while (flag >= 0 && a[flag] > val)
+                {
+                    a[flag + 1] = a[flag];
+                    flag--;
+                }
+                a[flag + 1] = val;
+            }
+            return a;
+        }
+    }
+}
---
+++ Ordinamento/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using MetodiOrdinamenti;
+using System.IO;
+
+namespace Ordinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "ordinamenti.csv";
+            using (StreamWriter w = new StreamWriter(file))
+            {
+                w.WriteLine("Algoritmo;Dimensione;Tempo");
+
+                Random rnd = new Random();
+                int[] dimensioni = { 100, 500, 1000, 10000, 15000, 20000,30000, 40000, 50000, 60000, 70000, 80000, 90000 };
+                foreach (int dim in dimensioni)
+                {
+                    int[] a = new int[dim];
+                    for (int i = 0; i < dim; i++)
+                    {
+                        a[i] = rnd.Next(0, 300000);
+                    }
+                    Stopwatch s = new Stopwatch();
+                    s.Start();
+                    a = MetodiOrdinamenti.InsertSort.Insert_Sort(a);
+                    s.Stop();
+                    long elapsed = s.ElapsedMilliseconds;
+                    w.WriteLine($"InsertSort;{dim};{elapsed}");
+                    Console.WriteLine($"{dim}");
+                    s.Reset();
+                    s.Start();
+                    a = MetodiOrdinamenti.BubbleSort.Bubble_Sort(a);
+                    s.Stop();
+                    elapsed = s.ElapsedMilliseconds;
+                    w.WriteLine($"BubbleSort;{dim};{elapsed}");
+                    Console.WriteLine($"{dim}");
+                }
+                w.Flush();
+            }
+        }
+    }
+}
